dab3407fd658fb2479ce2af6f69e91bcbd40c9f1
Rayyan Sameer <turbocharged.tomato@gmail.com>
2025-08-19 11:16:58 +0530

    Fixed Flask app issue in app.py

--- ci_cd-flask-app/app.py
+++ ci_cd-flask-app/app.py
@@ -8,6 +8,8 @@ def home():
     print("I am doing this to test if my changes are being reflected live")
     print("it works !")
 
+    return "Changes are live , check logs "
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
