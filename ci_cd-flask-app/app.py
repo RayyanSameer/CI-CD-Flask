9ee6f1797fecded3cd263447103e227737c90d34
Rayyan Sameer <turbocharged.tomato@gmail.com>
2025-08-19 13:00:38 +0530

    Fixed print issues fom line 6 through 11

--- ci_cd-flask-app/app.py
+++ ci_cd-flask-app/app.py
@@ -3,13 +3,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    print("This is a modded peice")
-    print("Here is another statement ")
-    print("I am doing this to test if my changes are being reflected live")
-    print("it works !")
+    return """This is a modded peice
+     "Here is another statement 
+     "I am doing this to test if my changes are being reflected live
+     "it works !
 
-    return "Changes are live , check logs "
-
+     "Changes are live , check logs """
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
