416c383f6f4021690ec620ee3d05a5a337b555f2
Rayyan Sameer <turbocharged.tomato@gmail.com>
2025-08-19 13:32:11 +0530

    Added additional testing features

--- ci_cd-flask-app/app.py
+++ ci_cd-flask-app/app.py
@@ -1,6 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route("/healthz")
+def healthz():
+    return "OK"
+
 @app.route("/")
 def home():
     return """This is a modded peice
--- ci_cd-flask-app/test.py
+++ ci_cd-flask-app/test.py
@@ -3,7 +3,7 @@ from app import app
 def test_home():
 	response = app.test_client().get("/")
 	assert response.status_code == 200
-	assert b"Hello DevOps" in response.data
+	assert b"Changes are live! " in response.data
 
 	
 	
